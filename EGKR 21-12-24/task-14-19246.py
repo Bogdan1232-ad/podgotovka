19361dfc1f347eb366bc4620ba5ca25bc9b06e15
BKovalev <kovalevvadim62@gmail.com>
2024-12-23 17:38:58 +0200

    8th exercise URA

---
+++ EGKR 21-12-24/task-14-19246.py
@@ -0,0 +1,9 @@
+from string import digits, ascii_uppercase
+
+alph = digits + ascii_uppercase
+
+for x in alph[:25]:
+    x1 = int('11353' + x + '12', 25)
+    x2 = int('135' + x + '21', 25)
+    if (x1 + x2) % 24 == 0:
+        print((x1 + x2) // 24)
--- EGKR 21-12-24/task-8-19240.py
+++ EGKR 21-12-24/task-8-19240.py
@@ -4,6 +4,6 @@ alph = sorted('январь')
 l = []
 for pos, val in enumerate(product(alph, repeat=5), 1):
     val = ''.join(val)
-    if val[0]!='я' and val.count('ь')<=1 and 'яя' not in val:
+    if val[0] != 'я' and val.count('ь') <= 1 and 'яя' not in val:
         l.append(pos)
 print(max(l))
