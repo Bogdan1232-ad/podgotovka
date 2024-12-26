40c27b0dcfd1a681dcc1775c23459dc5641e7f14
BKovalev <kovalevvadim62@gmail.com>
2024-12-26 17:38:17 +0200

    I have done smt

--- 261224/task-2-4581.py
+++ 261224/task-2-4581.py
@@ -1,11 +1,9 @@
 from itertools import permutations
 
-graph = 'VD DZ ZE EG GD BG AB AV BV'.split()
-matrix = '67 567 456 35 234 123 12'.split()
+graph = 'AD DE AF AB BE BF FC CG EG'.split()
+matrix = '37 367 125 56 34 247 126'.split()
 
 print(*range(1, 8))
-
-for i in permutations('ABGEVDZ'):
+for i in permutations('ABCDEFG'):
     if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
         print(*i)
-
