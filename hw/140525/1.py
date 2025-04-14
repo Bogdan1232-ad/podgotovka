8cc8203e4f9a720f43d13b4cb6bbf94326e81c5a
Bogdan1232-ad <81877401+Bogdan1232-ad@users.noreply.github.com>
2025-04-14 10:50:21 +0200

    Add files via upload

--- hw/140525/1.py
+++ hw/140525/1.py
@@ -1,1 +1,10 @@
+from itertools import permutations
 
+graph = 'АБ АГ АД БД ГД ГД ВГ ДЕ ВЗ ВЖ ЕЗ ЖЗ'.split()
+matrix = '245 15 478 135 1246 58 38 367'.split()
+
+print(*range(1, 9))
+
+for i in permutations('АБВГДЕЖЗ'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
